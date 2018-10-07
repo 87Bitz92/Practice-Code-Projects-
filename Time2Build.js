568137d4811ab32e3a481f29944a173f2bcf2b5d
Tina Araujo <itaraujo8720@gmail.com>
2018-10-07 14:01:07 -0500

    Add initail source code for practice projects

---
+++ Time2Build.js
@@ -0,0 +1,10 @@
+//const buttonProcras= document.getElementById ('button.procrastination');
+//const buttonCoding = document.getElementById('button.coding');
+//const buttonDepress = document.getElementById('button.depression');
+//const buttonself  = document.getElementById('button.selfimprovement');
+
+//buttonself.addEventListener('click', () => {
+	
+	//};
+
+//alert("Hello");
